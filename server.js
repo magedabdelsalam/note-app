3f6eabffa14a4c5bde118fa3a5bdc8ff7d634083
Maged Abdelsalam <dagadego@gmail.com>
2020-10-11 17:05:35 -0700

    Fix routing issue for heroku

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
@@ -23,6 +20,10 @@ app.get("/notes", (req, res) => {
 // Get notes
 app.get("/api/notes", (req, res) => {
   return res.json(readDB());
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // Post notes
